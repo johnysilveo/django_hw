74319ade343bb908d980714925efc3ed3e835e54
johnysilveo <vlad777z@icloud.com>
2025-09-19 12:27:10 -1000

    Fixed minor content view issues.

--- helloweb/urls.py
+++ helloweb/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path("contacts/", views.contacts, name="contacts"),
     path("index/", views.index, name="index"),
     path("song_string/",views.song_string, name="song_string"),
-
-    # history pages directly here
     path("history/", views.history_index, name="history_index"),
     path("history/people/", views.history_people, name="history_people"),
     path("history/photos/", views.history_photos, name="history_photos"),
